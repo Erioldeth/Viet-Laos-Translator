ae0d31ed45567898fc05f8e3fccc973494c31aaf
Erioldeth <crystaleye005@gmail.com>
2023-12-13 15:16:36 +0700

    remake encoder and decoder to make them look like original ones

--- module/layer/decoder.py
+++ module/layer/decoder.py
@@ -10,14 +10,11 @@ class Decoder(nn.Module):
 		super().__init__()
 
 		self.tok_embed = nn.Embedding(output_dim, d_model)
-		# self.pos_embed = nn.Embedding(max_len, d_model)
 		self.pe = PositionalEncoding(d_model, dropout, max_len)
 
 		self.layers = nn.ModuleList([DecoderLayer(d_model, heads, dropout) for _ in range(N)])
 
 		self.out = nn.Linear(d_model, output_dim)
-
-		# self.dropout = nn.Dropout(dropout)
 
 		self.device = device
 
@@ -28,13 +25,6 @@ class Decoder(nn.Module):
 		# memory = [batch_size, src_len, d_model]
 		# src_mask = [batch_size, 1, 1, src_len]
 		# trg_mask = [batch_size, 1, trg_len, trg_len]
-		# bs, trg_len = trg.shape
-		#
-		# pos = torch.arange(0, trg_len)[None].repeat(bs, 1).to(self.device)
-		# # pos = [batch_size, trg_len]
-		#
-		# x = self.dropout(self.tok_embed(trg) * self.scale + self.pos_embed(pos))
-		# # x = [batch_size, trg_len, d_model]
 
 		x = self.pe(self.tok_embed(trg) * self.scale)
 		# x = [batch_size, trg_len, d_model]
@@ -42,7 +32,6 @@ class Decoder(nn.Module):
 		attn = None
 		for layer in self.layers:
 			x, attn = layer(x, memory, src_mask, trg_mask)
-		# x = [batch_size, trg_len, d_model]
 		# attn = [batch_size, heads, trg_len, src_len]
 
 		x = self.out(x)
@@ -54,10 +43,7 @@ class Decoder(nn.Module):
 class DecoderLayer(nn.Module):
 	def __init__(self, d_model, heads, dropout):
 		super().__init__()
-
-		self.sa_norm = nn.LayerNorm(d_model)
-		self.enc_norm = nn.LayerNorm(d_model)
-		self.ff_norm = nn.LayerNorm(d_model)
+		self.norm = nn.LayerNorm(d_model)
 
 		self.self_attn = MultiHeadAttention(heads, d_model, dropout)
 		self.enc_attn = MultiHeadAttention(heads, d_model, dropout)
@@ -70,20 +56,18 @@ class DecoderLayer(nn.Module):
 		# memory = [batch_size, src_len, d_model]
 		# src_mask = [batch_size, 1, 1, src_len]
 		# trg_mask = [batch_size, 1, trg_len, trg_len]
-		trg_sa, _ = self.self_attn(trg, trg, trg, trg_mask)
 
-		trg = self.sa_norm(trg + self.dropout(trg_sa))
-		# trg = [batch_size, trg_len, d_model]
+		x = self.norm(trg)
+		x, _ = self.self_attn(x, x, x, trg_mask)
+		trg = trg + self.dropout(x)
 
-		trg_ea, attn = self.enc_attn(trg, memory, memory, src_mask)
-
-		trg = self.enc_norm(trg + self.dropout(trg_ea))
-		# trg = [batch_size, trg_len, d_model]
-
-		trg_ff = self.ff(trg)
-
-		trg = self.ff_norm(trg + self.dropout(trg_ff))
-		# trg = [batch_size, trg_len, d_model]
+		x = self.norm(trg)
+		x, attn = self.enc_attn(x, memory, memory, src_mask)
 		# attn = [batch_size, heads, trg_len, src_len]
+		trg = trg + self.dropout(x)
+
+		x = self.norm(trg)
+		x = self.ff(x)
+		trg = trg + self.dropout(x)
 
 		return trg, attn
--- module/layer/encoder.py
+++ module/layer/encoder.py
@@ -10,12 +10,9 @@ class Encoder(nn.Module):
 		super().__init__()
 
 		self.tok_embed = nn.Embedding(input_dim, d_model)
-		# self.pos_embed = nn.Embedding(max_len, d_model)
 		self.pe = PositionalEncoding(d_model, dropout, max_len)
 
 		self.layers = nn.ModuleList([EncoderLayer(d_model, heads, dropout) for _ in range(N)])
-
-		# self.dropout = nn.Dropout(dropout)
 
 		self.device = device
 
@@ -24,20 +21,12 @@ class Encoder(nn.Module):
 	def forward(self, src, src_mask):
 		# src = [batch_size, src_len]
 		# src_mask = [batch_size, 1, 1, src_len]
-		# bs, src_len = src.shape
-
-		# pos = torch.arange(0, src_len)[None].repeat(bs, 1).to(self.device)
-		# # pos = [batch_size, src_len]
-		#
-		# x = self.dropout(self.tok_embed(src) * self.scale + self.pos_embed(pos))
-		# # x = [batch_size, src_len, d_model]
 
 		x = self.pe(self.tok_embed(src) * self.scale)
 		# x = [batch_size, src_len, d_model]
 
 		for layer in self.layers:
 			x = layer(x, src_mask)
-		# x = [batch_size, src_len, d_model]
 
 		return x
 
@@ -45,9 +34,7 @@ class Encoder(nn.Module):
 class EncoderLayer(nn.Module):
 	def __init__(self, d_model, heads, dropout):
 		super().__init__()
-
-		self.sa_norm = nn.LayerNorm(d_model)
-		self.ff_norm = nn.LayerNorm(d_model)
+		self.norm = nn.LayerNorm(d_model)
 
 		self.attn = MultiHeadAttention(heads, d_model, dropout)
 		self.ff = FeedForward(d_model, dropout)
@@ -58,14 +45,12 @@ class EncoderLayer(nn.Module):
 		# src = [batch_size, src_len, d_model]
 		# src_mask = [batch_size, 1, 1, src_len]
 
-		src_sa, _ = self.attn(src, src, src, src_mask)
+		x = self.norm(src)
+		x, _ = self.attn(x, x, x, src_mask)
+		src = src + self.dropout(x)
 
-		src = self.sa_norm(src + self.dropout(src_sa))
-		# src = [batch_size, src_len, d_model]
-
-		src_ff = self.ff(src)
-
-		src = self.ff_norm(src + self.dropout(src_ff))
-		# src = [batch_size, src_len, d_model]
+		x = self.norm(src)
+		x = self.ff(x)
+		src = src + self.dropout(x)
 
 		return src
